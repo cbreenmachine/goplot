4b885341100871cbd4b222ad05ae732ae196212e
Coleman Breen <ceb@cebs-m1.local>
2022-11-23 21:03:46 -0600

    still figuring out namespace with Org.hs.db

---
+++ plot_go_barchart.R
@@ -0,0 +1,25 @@
+#' Plot gene ontology outputs
+#'@param go.df a data frame with at least Ontology, Description, and p.adjust
+#'
+#'@details
+#'Cleaner looking version
+#'@import ggplot2
+#' @export
+
+
+plot_go_barchart <- function(go.df, n=30){
+  ggplot(data = head(go.df, n),
+         aes(x = Description,
+             y = -log10(p.adjust),
+             fill = Ontology)) +
+    geom_bar(stat = "identity") +
+    coord_flip() +
+    theme_minimal(base_size = 20) +
+    xlab("") +
+    ylab(expression(-log[10](p))) +
+    labs(legend = "") +
+    theme(legend.position = "top",
+          plot.background = element_rect(fill = "white", color = "white"))
+  #TODO: colors
+  # + scale_fill_manual() or something
+}
